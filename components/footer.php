d4d764e28c5f26a5df2a0787d2baf8915dd73974
nurulazizah06 <117329645+nurulazizah06@users.noreply.github.com>
2022-12-07 22:04:16 +0700

    edit footer

--- components/footer.php
+++ components/footer.php
@@ -9,24 +9,23 @@
       </div>
 
       <div class="box">
-         <img src="images/clock-icon.png" alt="">
+         <img src="images/alamat social-media.png" alt="">
          <h3>Our Social Media</h3>
          <a href="https://instagram.com/fanny_makeupwedding?igshid=ZmVmZTY5ZGE=">Instagram : fanny_makeupwedding</a>
          <a href="https://www.tiktok.com/@fannysulistiowati?_t=8XyWlTeBsn3&_r=1">Tiktok : fanny_makeup</a>
       </div>
 
       <div class="box">
-         <img src="images/map-icon.png" alt="">
-         <h3>Our Address</h3>
-         <a href="https://www.google.com/maps/place/8%C2%B022'02.4%22S+113%C2%B038'29.9%22E/@-8.3673267,113.6394358,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xceb6403cd55bc3c4!8m2!3d-8.3673267!4d113.6416245?hl=id
-">Ambulu - Jember</a>
-      </div>
-
-      <div class="box">
-         <img src="images/phone-icon.png" alt="">
+         <img src="images/alamat whatsapp.png" alt="">
          <h3>Our Number</h3>
          <a href="whatsapp://send?text=Halo Kak&phone=+6282161171191">082161171191</a>
          <a href="whatsapp://send?text=Halo Kak&phone=+6282244442422">082244442422</a>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our Address</h3>
+         <a href="https://www.google.com/maps/place/8%C2%B022'02.4%22S+113%C2%B038'29.9%22E/@-8.3673267,113.6394358,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xceb6403cd55bc3c4!8m2!3d-8.3673267!4d113.6416245?hl=id">Ambulu - Jember</a>
       </div>
 
    </section>
