261afeb3842f9a554e831e85b5b496c9207ac15a
RicoGanteng-90 <rahmandarico3@gmail.com>
2022-12-27 15:49:36 +0700

    Overlay hilang

--- menu.php
+++ menu.php
@@ -74,34 +74,23 @@ include 'components/add_cart.php';
          <input type="hidden" name="keterangan" value="<?= $fetch_products['keterangan']; ?>">
          <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
          <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
-         
-         <div class="card">
-         <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
-         <div class="card__overlay">
          <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
          <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
-            <div class="overlay__text">            
-            <div class="keterangan"><?= $fetch_products['keterangan'];?></div>
-            </div>
-          </div>
-         
-         <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
-         <div class="name"><?= $fetch_products['name']; ?></div>
-         
+         <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+         <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>        
+         <div class="name"><?= $fetch_products['name']; ?></div>         
          <div class="flex">
             <div class="price"><span>Rp. </span><?= $fetch_products['price']; ?></div>
             <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
          </div>                  
-      </form>
-      </div>
+      </form>      
       <?php
             }
          }else{
             echo '<p class="empty">tidak ada produk yang ditambahkan!</p>';
          }
       ?>
-
-   </div>
+      </div>
 </section>
 <!-- menu section ends -->
 
