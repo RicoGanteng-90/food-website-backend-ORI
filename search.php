e544442af22fa7a0fbb7c46cc7976c8f87a6fdb7
RicoGanteng-90 <rahmandarico3@gmail.com>
2022-12-31 14:57:16 +0700

    edit Harga

--- search.php
+++ search.php
@@ -85,7 +85,7 @@ include 'components/add_cart.php';
          <div class="name"><?= $fetch_products['name']; ?></div>
          <div class="keterangan"></div>
          <div class="flex">
-            <div class="price"><span>$</span><?= $fetch_products['price']; ?></div>
+         <div class="price"><span>Rp. </span><?php echo " " . number_format($fetch_products['price'],0,',','.'); ?><span></span></div>
             <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
          </div>
       </form>
